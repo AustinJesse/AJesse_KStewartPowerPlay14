a2adfe6de3f48252964a7210f1ae62fb74bf3d58
Ky <43182468+kylota@users.noreply.github.com>
2020-07-21 10:38:14 -0500

    UnitTest1 Successful

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -1,5 +1,8 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../Player.h"
+#include "../Library.h"
+#include "../Object.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,8 +12,34 @@ namespace UnitTest1
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(LevelUpTest)
 		{
+			Player player;
+			Assert::AreEqual(1, player.getLevel());
+			Assert::IsTrue(player.getHealth() > 0 && player.getStrength() > 0);
+
+			int level = player.getLevel();
+			int health = player.getHealth();
+			int strength = player.getStrength();
+			player.levelUp();
+			Assert::IsTrue(level < player.getLevel());
+			Assert::IsTrue(health < player.getHealth());
+			Assert::IsTrue(strength < player.getStrength());
 		}
+
+		/*TEST_METHOD(AbilityTest)
+		{
+			Library library;
+			std::vector<std::unique_ptr<Object>> objects;
+
+			library.inventory["Player"] = std::make_unique<PlayerFactory>();
+			library.inventory["Monster1"] = std::make_unique<MonsterFactory>();
+			library.inventory["Monster2"] = std::make_unique<MonsterFactory>();
+			library.inventory["Monster1"] = std::make_unique<MonsterFactory>();
+
+			for (const auto& object : library.inventory) {
+				objects.emplace_back(object.second);
+			}
+		}*/
 	};
 }
