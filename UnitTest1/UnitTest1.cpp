39b0c50f828e344bef19a353705bc669e4428d58
Ky <43182468+kylota@users.noreply.github.com>
2020-07-21 11:59:41 -0500

    Update UnitTest1.cpp
    Added for range for abilities

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -3,6 +3,9 @@
 #include "../Player.h"
 #include "../Library.h"
 #include "../Object.h"
+#include "../AttackStrategy.h"
+#include "../HealStrategy.h"
+#include "../FuryStrategy.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -27,7 +30,7 @@ namespace UnitTest1
 			Assert::IsTrue(strength < player.getStrength());
 		}
 
-		/*TEST_METHOD(AbilityTest)
+		TEST_METHOD(AbilityTest)
 		{
 			Library library;
 			std::vector<std::unique_ptr<Object>> objects;
@@ -35,11 +38,25 @@ namespace UnitTest1
 			library.inventory["Player"] = std::make_unique<PlayerFactory>();
 			library.inventory["Monster1"] = std::make_unique<MonsterFactory>();
 			library.inventory["Monster2"] = std::make_unique<MonsterFactory>();
-			library.inventory["Monster1"] = std::make_unique<MonsterFactory>();
+			library.inventory["Monster3"] = std::make_unique<MonsterFactory>();
 
 			for (const auto& object : library.inventory) {
 				objects.emplace_back(object.second);
 			}
-		}*/
+
+			Player player2;
+			while (player2.getLevel() < 5) {
+				player2.levelUp();
+			}
+			Assert::IsTrue(player2.abilities.size() == 5);
+			int level = player2.getLevel();
+			int strength = player2.getStrength();
+			int SP = player2.getSP();
+
+			for (const auto& ability : player2.abilities)
+			{
+				ability->execute(objects);
+			}			
+		}
 	};
 }
