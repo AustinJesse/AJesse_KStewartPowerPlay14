230723455be43d73d067024077f9a04091c24be7
AustinJesse <aajesse@ualr.edu>
2020-07-21 12:05:38 -0500

    mn mn

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -3,6 +3,10 @@
 #include "../Player.h"
 #include "../Library.h"
 #include "../Object.h"
+#include "../AttackStrategy.h"
+#include "../HealStrategy.h"
+#include "../FuryStrategy.h"
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -27,19 +31,33 @@ namespace UnitTest1
 			Assert::IsTrue(strength < player.getStrength());
 		}
 
-		/*TEST_METHOD(AbilityTest)
+		TEST_METHOD(AbilityTest)
 		{
 			Library library;
 			std::vector<std::unique_ptr<Object>> objects;
-
 			library.inventory["Player"] = std::make_unique<PlayerFactory>();
 			library.inventory["Monster1"] = std::make_unique<MonsterFactory>();
 			library.inventory["Monster2"] = std::make_unique<MonsterFactory>();
-			library.inventory["Monster1"] = std::make_unique<MonsterFactory>();
-
+			library.inventory["Monster3"] = std::make_unique<MonsterFactory>();
 			for (const auto& object : library.inventory) {
 				objects.emplace_back(object.second);
 			}
-		}*/
+
+			/*while (player2.getLevel() < 5) {
+				player2.levelUp();
+			}*/
+			Player player;
+			player.levelUp();
+			player.levelUp();
+			player.levelUp();
+			player.levelUp();
+			Assert::IsTrue(player.abilities.size() == 5);
+			int level = player.getLevel();
+			int strength = player.getStrength();
+			int SP = player.getSP();
+
+
+
+		}
 	};
 }
